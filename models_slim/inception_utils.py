fde7bee9c434ab87d8c2a6ba5f97312810ce104b
Paul Balanca <paul.balanca@gmail.com>
2017-05-17 13:01:08 +0100

    FIX: spatial squeeze arg scope.

--- models_slim/inception_utils.py
+++ models_slim/inception_utils.py
@@ -79,6 +79,7 @@ def inception_arg_scope(weight_decay=0.00004,
             with slim.arg_scope([slim.conv2d, slim.max_pool2d, slim.avg_pool2d,
                                  custom_layers.concat_channels,
                                  custom_layers.channel_to_last,
+                                 custom_layers.spatial_squeeze,
                                  custom_layers.ksize_for_squeezing],
                                 data_format=data_format) as sc:
                 return sc
