FIX: image scaling: divide by tf.uint8.max, i.e. 255 not 256.

models_slim/inception_utils.py
@@ -35,7 +35,7 @@ def inception_pre_rescaling(images, is_training=True):
     Input tensor supposed to be in [0, 256) range.
     """
     # Rescale to [-1,1] instead of [0, 1)
-    images *= 1. / 256
+    images *= 1. / 255
     images = tf.subtract(images, 0.5)
     images = tf.multiply(images, 2.0)
     return images

preprocessing.py
@@ -275,7 +275,7 @@ def distort_image(image, height, width, bbox, thread_id=0, scope=None):
         # Randomly distort the colors.
         distorted_image = distort_color(distorted_image, thread_id)
         # Note: This ensures the scaling matches the output of eval_image
-        distorted_image *= 256
+        distorted_image *= 255
 
         if not thread_id:
             tf.summary.image('final_distorted_image',