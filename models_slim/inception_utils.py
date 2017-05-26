bb8fdcb344871c1a708d23b814b87c488ffc7b9a
Paul Balanca <paul.balanca@gmail.com>
2017-05-26 11:31:43 +0100

    UPDATE: Inception pre-rescaling.

--- models_slim/inception_utils.py
+++ models_slim/inception_utils.py
@@ -35,9 +35,11 @@ def inception_pre_rescaling(images, is_training=True):
     Input tensor supposed to be in [0, 256) range.
     """
     # Rescale to [-1,1] instead of [0, 1)
-    images *= 1. / 255
-    images = tf.subtract(images, 0.5)
-    images = tf.multiply(images, 2.0)
+    # images *= 1. / 255
+    # images = tf.subtract(images, 0.5)
+    # images = tf.multiply(images, 2.0)
+    images -= 127.5
+    images *= 1. / 127.5
     return images
 
 
