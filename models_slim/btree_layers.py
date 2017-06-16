3d7cb7283ce0fb3cc8b6ea2b9b35c266951953f2
Paul Balanca <paul.balanca@gmail.com>
2017-06-16 23:42:08 +0100

    ADD: B-tree layer.

---
+++ models_slim/btree_layers.py
@@ -0,0 +1,43 @@
+# Copyright 2017 Paul Balanca. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Some custom layers, implementing random and ludicrous ideas
+I sometimes have.
+"""
+import math
+import numpy as np
+import tensorflow as tf
+
+import functools
+
+from tensorflow.contrib.framework.python.ops import add_arg_scope
+from tensorflow.contrib.framework.python.ops import variables
+from tensorflow.contrib.layers.python.layers import initializers
+from tensorflow.contrib.layers.python.layers import utils
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.ops import standard_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.training import moving_averages
+from tensorflow.python.layers import convolutional as convolutional_layers
+from tensorflow.python.ops import variables as tf_variables
+
+slim = tf.contrib.slim
+
